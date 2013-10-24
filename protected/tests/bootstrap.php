f189da44c9d8dbe37e310eab9773a770e6d8c57b
u13pro <u13pro@u13pro-W35-37ET>
2013-10-24 20:36:13 +0100

    Configuracion Bootstrap.php para que genere los UnitTest

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
 
 // change the following paths if necessary
-$yiit=dirname(__FILE__).'/../../../../../../../opt/lampp/htdocs/yii-1.1.14.f0fee9/framework/yiit.php';
+$yiit=dirname(__FILE__).'/../../../../../../../opt/lampp/htdocs/yii/framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
+date_default_timezone_set('America/Los_Angeles');
+
 require_once($yiit);
+require_once (Yii::getPathOfAlias('system.test.CTestCase').'.php');
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
 Yii::createWebApplication($config);
